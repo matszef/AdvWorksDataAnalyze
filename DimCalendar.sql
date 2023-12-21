50d123e813da0ce55febeca374f96974bcd0ef32
matszef <59147408+matszef@users.noreply.github.com>
2023-12-21 07:02:26 +0100

    Cleaned calendar table

---
+++ DimCalendar.sql
@@ -0,0 +1,26 @@
+-- cleaned DimDateTable --
+SELECT 
+  TOP (1000) [DateKey], 
+  [FullDateAlternateKey] as Date,
+  --,[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] as Day,
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear], 
+  [WeekNumberOfYear] as WeekNr, 
+  [EnglishMonthName] as Month,
+  left([EnglishMonthName], 3) as MonthShort,
+  --,[SpanishMonthName]
+  --,[FrenchMonthName] 
+  [MonthNumberOfYear] as MonthNr, 
+  [CalendarQuarter] as Quarter, 
+  [CalendarYear] as Year 
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2016_EXT].[dbo].[DimDate]
+  WHERE CalendarYear >= 2019
+
